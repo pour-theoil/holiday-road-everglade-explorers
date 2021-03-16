f8d3a104390f09661470d8458fb4360b8d0bcbcf
Chris Douglass <cdouglass516@gmail.com>
2021-03-16 11:52:26 -0500

    Added filtering for Attactions and Eateries

--- scripts/attractions/AttractionProvider.js
+++ scripts/attractions/AttractionProvider.js
@@ -2,7 +2,13 @@ export const getAttractions = () => {
     return fetch("http://holidayroad.nss.team/bizarreries")
     .then(response => response.json())
     .then(parsedResponse => { 
-    return parsedResponse;
-    })
+        let AttractionsArray = [];
+        parsedResponse.forEach((item) => {
+            if (item.state === "NC" || item.state === "TN" || item.state === "VA"|| item.state === "SC") {
+                AttractionsArray.push(item);
+            }
+            })
+            return AttractionsArray;
+    });
     }
 
